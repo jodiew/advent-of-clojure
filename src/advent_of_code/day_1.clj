9257bce19d89cd05618f0074a61af00fb58c375a
Jodie Weldon <weldon.jodie@gmail.com>
2022-12-02 12:41:03 -0800

    Clean up and add testing

---
+++ src/advent_of_code/day_1.clj
@@ -0,0 +1,36 @@
+(ns advent-of-code.day-1 
+  (:require [clojure.string :as string]))
+
+;; Part 1
+
+(defn parse-input [input]
+  (->> input
+       string/split-lines
+       (map #(when (not-empty %) (Integer/parseInt %)))
+       (partition-by nil?)
+       (filter #(first %))))
+
+(defn sum-calories [elves]
+  (map #(reduce + %) elves))
+
+(defn part-1 [input]
+  (->> input
+      parse-input
+      sum-calories
+      (apply max)))
+
+(part-1 (slurp "resources/day_1_input.txt"))
+;; => 70613
+
+;; Part 2
+
+(defn part-2 [input]
+  (->> input
+       parse-input
+       sum-calories
+       (sort >)
+       (take 3)
+       (reduce +)))
+
+(part-2 (slurp "resources/day_1_input.txt"))
+;; => 205805
---
+++ test/advent_of_code/day_1_test.clj
@@ -0,0 +1,46 @@
+(ns advent-of-code.day-1-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [advent-of-code.day-1 :refer [parse-input sum-calories part-1 part-2]]))
+
+(def test-input "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000")
+
+(deftest parse-input-test
+      (testing "Parse the string input"
+        (is (= [[1000 2000 3000]
+                [4000]
+                [5000 6000]
+                [7000 8000 9000]
+                [10000]]
+               (parse-input test-input)))))
+
+(deftest sum-calories-test
+      (testing "List the total calories each elf is carrying"
+        (is (= [6000 4000 11000 24000 10000]
+               (sum-calories [[1000 2000 3000]
+                              [4000]
+                              [5000 6000]
+                              [7000 8000 9000]
+                              [10000]])))))
+
+(deftest part-1-test
+      (testing "Find the largest amount of calories carried by an elf"
+        (is (= 24000
+               (part-1 test-input)))))
+
+(deftest part-2-test
+      (testing "The number of calories carried by the top 3 elves"
+        (is (= 45000
+               (part-2 test-input)))))
